8db29ff55e2130fe8c593bfa9e72bffb0e77e9d5
Jonathan Arteaga <andresmartines409@gmail.com>
2025-09-05 11:08:41 -0500

    dbeaver insertar usuario

--- beta.sql
+++ beta.sql
@@ -21,3 +21,30 @@ CREATE TABLE users(id SERIAL PRIMARY KEY, ide_number text not null, firstname te
 -- queri par ainsertar datos 
 
 insert into users(ide_number,firstname,lastname,mobile_number,email,password) values('87067555','joan','ayala','3122429021','joan@mail.com','1234');
+
+--select * from users;
+/*
+insert into users(ide_number,
+firstname,
+lastname,
+mobile_number,
+email,password
+) values(
+'87067555',
+'jovani',
+'ayala',
+'3122429021',
+'jofdfdaan@mail.com',
+'1dsfd234'
+);
+*/
+--si seleccionas y das ejecutar se ejecuta aun comentariado 
+
+select 
+u.firstname as nombres,
+u.lastname as apellidos,
+u.firstname ||' '|| u.lastname as fullname,  --selecciona los datos, u es apra el alias as para cambiar nomenclatura
+u.email,
+case when u.status = true then 'Activo' else ' inactivo' end 
+from 
+	users u ;
